ca492678d162bd06a32160eec7b4668cccee03a2
Marcello Duarte <marcello.duarte@gmail.com>
2025-10-14 22:00:02 +0100

    Fix CI

--- examples/concurrency.php
+++ examples/concurrency.php
@@ -4,12 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Phunkie\Effect\Concurrent\FiberExecutionContext;
 use Phunkie\Effect\Concurrent\ParallelExecutionContext;
-
 use Phunkie\Validation\Failure;
 use Phunkie\Validation\Success;
-use function Phunkie\Effect\Functions\io\io;
-
 use Phunkie\Streams\Network;
+use function Phunkie\Effect\Functions\io\io;
 use function Phunkie\Functions\show\show;
 
 echo "=== Phunkie Streams Concurrency Examples ===\n\n";
