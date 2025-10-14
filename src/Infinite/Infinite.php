9dcb76e551a0b1b8fbc769c1ca9128bea316dcc5
Marcello Duarte <marcello.duarte@gmail.com>
2025-10-14 18:58:25 +0100

    Add copyright header

--- src/Infinite/Infinite.php
+++ src/Infinite/Infinite.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Phunkie Streams, a PHP functional library to work with Streams.
+ *
+ * (c) Marcello Duarte <marcello.duarte@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phunkie\Streams\Infinite;
 
 interface Infinite
